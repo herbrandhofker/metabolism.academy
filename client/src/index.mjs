af3705e1cc123f02790ddfb7f973d36957fcbbcd
Herbrand Hofker <herbrand.hofker@gmail.com>
2021-05-16 12:41:32 +0200

    aa

--- client/src/index.mjs
+++ client/src/index.mjs
@@ -3,12 +3,3 @@ import './index.css'
 import './the-others.mjs';
 import './login.mjs'
 
-/*
-const createChat=function () {
-  const item = root.appendChild(document.createElement("group-chat"))
-  item.id = "Chat";
-  item.classList.add("menuItem");
-  item.style.display = "none"
-  return item;
-}
-*/
