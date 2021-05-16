08c065a1f8981ee3066a8ea31882ef2c5b972c3e
Herbrand Hofker <herbrand.hofker@gmail.com>
2021-05-16 12:44:02 +0200

    aa

--- client/src/index.mjs
+++ client/src/index.mjs
@@ -3,3 +3,16 @@ import './index.css'
 import './the-others.mjs';
 import './login.mjs'
 
+
+const dialogCloseBtn =document.getElementById("dialogCloseBtn");
+const dialogDiv =document.getElementById("dialogDiv");
+
+dialogCloseBtn.addEventListener("click",()=>dialogDiv.style.display="none")
+
+
+const dialogHeader =document.getElementById("dialogHeader");
+dialogHeader.innerText="abc";
+const dialogContent =document.getElementById("dialogContent");
+dialogContent.innerText="Lorem"
+const dialogFooter =document.getElementById("dialogFooter");
+dialogFooter.innerHTML="<h3>xyz</h3>";
