dc9e03d436cef0408e51fbde78178cf904b0a043
Herbrand Hofker <herbrandhofker@users.noreply.github.com>
2021-05-25 10:34:11 +0200

    aa

--- client/src/youtube-button.mjs
+++ client/src/youtube-button.mjs
@@ -1,5 +1,5 @@
 import { LitElement, html, css, svg } from 'lit-element';
-import {youtubeDialog} from './youtube-dialog.mjs'
+import { youtubeDialog } from './youtube-dialog.mjs'
 const svgWidth = 30;
 const svgHeight = svgWidth;
 const closeSvg = `<svg width="` + svgWidth + `" height="` + svgHeight + `" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1490 1322q0 40-28 68l-136 136q-28 28-68 28t-68-28l-294-294-294 294q-28 28-68 28t-68-28l-136-136q-28-28-28-68t28-68l294-294-294-294q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 294 294-294q28-28 68-28t68 28l136 136q28 28 28 68t-28 68l-294 294 294 294q28 28 28 68z"/></svg>`;
@@ -56,7 +56,7 @@ class YoutubeVideo extends LitElement {
 
     render() {
         return html`
-        <svg id="button" @click="${()=> this.createVideo()}" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="youtube-square"
+        <svg id="button" @click="${() => this.createVideo()}" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="youtube-square"
             class="svg-inline--fa fa-youtube-square fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 448 512">
          <path d="M186.8 202.1l95.2 54.1-95.2 54.1V202.1zM448 80v352c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V80c0-26.5 21.5-48 48-48h352c26.5 0 48 21.5 48 48zm-42 176.3s0-59.6-7.6-88.2c-4.2-15.8-16.5-28.2-32.2-32.4C337.9 128 224 128 224 128s-113.9 0-142.2 7.7c-15.7 4.2-28 16.6-32.2 32.4-7.6 28.5-7.6 88.2-7.6 88.2s0 59.6 7.6 88.2c4.2 15.8 16.5 27.7 32.2 31.9C110.1 384 224 384 224 384s113.9 0 142.2-7.7c15.7-4.2 28-16.1 32.2-31.9 7.6-28.5 7.6-88.1 7.6-88.1z">
@@ -65,7 +65,12 @@ class YoutubeVideo extends LitElement {
     }
 
     createVideo() {
-        const videoDialog = getStaticVideo(this.videoData.id);
+        let videoDialog = null;
+        if (configs.has(this.videoData.id)) {
+            videoDialog = configs.get(this.videoData.id).videoDialog;
+        }
+        else
+            videoDialog = getStaticVideo(this.videoData.id);
 
         youtubeDialog.style.display = "block";
         youtubeDialog.title = this.title;
@@ -82,16 +87,11 @@ class YoutubeVideo extends LitElement {
         youtubeDialog.content.appendChild(videoDialog)
 
         function getStaticVideo(videoId) {
-
-            if (configs.has(videoId)) {
-                return configs.get(videoId).videoDialog;
-            }
-
             const ytVideoContainer = document.createElement("div");
             ytVideoContainer.classList.add("youtube-video-container")
             const video = ytVideoContainer.appendChild(document.createElement("video"));
-            video.controls=false;
-      
+            video.controls = false;
+
             const config = { data: {} };
             config.videoDialog = ytVideoContainer;
             config.video = video;
